c4ebca5d3ceb6618db42f96c9d64e180fd53b4f3
Ruben Gees <rubengees7@googlemail.com>
2018-06-09 16:32:17 +0200

    Fix error due to new empty response

--- app/src/main/java/de/smartsquare/kickchain/api/KickchainApi.kt
+++ app/src/main/java/de/smartsquare/kickchain/api/KickchainApi.kt
@@ -9,5 +9,5 @@ import retrofit2.http.POST
 interface KickchainApi {
 
     @POST("/game/new")
-    fun createGame(@Body game: Game): Single<Response<Any>>
+    fun createGame(@Body game: Game): Single<Response<Void>>
 }
